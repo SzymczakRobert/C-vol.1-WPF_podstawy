417159c2b20285ba4853c933b05d3fb12f98dffa
SzymczakRobert <97690534+SzymczakRobert@users.noreply.github.com>
2022-11-29 20:39:09 +0100

    Add files via upload

---
+++ 40 - WPF-XAML różnice, aplikacja/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int Counter {get; private set;}
+        public MainWindow()
+        {
+            InitializeComponent();
+            Counter = 0;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            label_content.Content = Counter++;
+        }
+    }
+}
---
+++ 41 - WPF - ważne pojęcia etc/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+
+    /*  Podstawowe kolejności Dziedziczenia z dołu do góry i na odwrót
+     *  tu jest poazane jak kombinowac z butonem w butonie
+     *  łączanie wyłaćzanie buttona IsEnabled="False" or True
+     *  FontFamily dziedziczenie czcionek itp fakt że nie wszystko ma tę funkcję
+     *  Umieszczanie obrazów
+     *  Umiejscawianie siatki w siatce
+     *  ustawianie marginesów
+     *  dodatkowe mesage boxy
+     *  */
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("Test");
+            test.Text = "zstało kliknięte";
+        }
+    }
+}
